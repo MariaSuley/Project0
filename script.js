7319e06462c8bd94e4ce9500d2cb5b5c3fd24f66
Maria <sahra-773@yandex.ru>
2020-07-25 17:52:22 +0500

    beginLesson1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const number0Films = +prompt('Сколько фильмов вы уже посмотрели?','');
+ 
+const personMowieDB = {
+    count: number0Films,
+    mowies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+      personMowieDB.mowies[a] = b;
+      personMowieDB.mowies[c] = d;
+
+      console.log(personMowieDB);
