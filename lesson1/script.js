777455e8474990fa267f74987742cec2365b0fd2
Maria <sahra-773@yandex.ru>
2020-07-29 16:27:50 +0500

    add script3 4

--- lesson1/script.js
+++ lesson1/script.js
@@ -8,12 +8,36 @@ const personMowieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько оцените его?','');
 
-      personMowieDB.mowies[a] = b;
-      personMowieDB.mowies[c] = d;
+//       personMowieDB.mowies[a] = b;
+//       personMowieDB.mowies[c] = d;
 
+
+      for (let i = 0; i < 2; i++) {
+        const c = prompt('Один из последних просмотренных фильмов?',''),
+              d = prompt('На сколько оцените его?','');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personMowieDB.mowies[c] = d;
+            console.log('done');
+        } else {
+            console.log('error')
+            i--;
+        }
+
+      }
+
+      if (personMowieDB.count < 10) {
+          console.log('Просмотрено довольно мало фильмов');
+      } else if (personMowieDB.count >= 10 && personMowieDB.count <= 30) {
+        console.log('Вы классный зритель');
+      } else if (personMowieDB.count >= 30) {
+          console.log('Вы киноман');
+      } else {
+          console.log('Произошла ошибка');
+      }
       console.log(personMowieDB);
--- lesson2/script2.js
+++ lesson2/script2.js
@@ -1,14 +1,14 @@
-" use strict";
+// " use strict";
 
-if (4 == 9) {
-    console.log('Ok');
-} else {
-    console.log('Error');
-}
+// if (4 == 9) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
 
-const num = 50;
+// const num = 50;
 
-(num === 50) ? console.log('Ok') : console.log('Error');
+// (num === 50) ? console.log('Ok') : console.log('Error');
 
 const num = 50;
 
